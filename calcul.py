c7b28e2c2e8d000deea78bd456d8910fde4fb2e1
srustikochalapurmath <srustikochalapurmath@gmail.com>
2025-10-28 11:54:31 +0530

    commited

---
+++ calcul.py
@@ -0,0 +1,15 @@
+# Program to perform arithmetic operations
+
+# Taking input from the user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# Performing operations
+print("\nArithmetic Operations:")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent (a^b):", a ** b)
